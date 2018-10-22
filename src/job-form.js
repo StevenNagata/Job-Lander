b626da24f47c68f5529658fd70765f5e5f12891a
StevenNagata <nagata.steven1@gmail.com>
2018-10-22 13:33:03 -0700

    Created a save button on the bottom of the form and when clicked sends an new object to the app.js with all the information. #1

--- src/job-form.js
+++ src/job-form.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true
+
 const options = [
   {
     id: 1,
@@ -37,85 +38,104 @@ export default class JobForm extends React.Component {
       status: 'Interested'
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleChange(event) {
     const status = event.target.value
     this.setState({ status })
-
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    const newProspect = {
+      company: event.target.company.value,
+      title: event.target.title.value,
+      description: event.target.description.value,
+      details: event.target.details.value,
+      status: event.target.status.value
+    }
+    event.target.reset()
+    console.log(newProspect)
   }
   render() {
     return (
       <div>
-        <Typography variant='h5' gutterBottom align='center'>
-          Create New Job Prospect
-        </Typography>
-        <TextField
-          required
-          fullWidth
-          id="comapny-name"
-          placeholder="Facebook"
-          label="Company Name"
-          margin="normal"
-        />
-        <TextField
-          required
-          fullWidth
-          id="job-title"
-          placeholder="e.g Software Developer"
-          label="Job Title"
-          margin="normal"
-        />
-        <TextField
-          id="description-input"
-          label="Description"
-          placeholder="Asking for candidate with 3+ years of Javascrip experience/ Two complete self projects..."
-          fullWidth
-          multiline
-          rows="5"
-          margin="normal"
-          variant="filled"
-        />
-        <TextField
-          id="details-input"
-          label="Details"
-          placeholder="Get exposure to more backend work. Work life balance not so great..."
-          fullWidth
-          multiline
-          rows="5"
-          margin="normal"
-          variant="filled"
-        />
-        <TextField
-          select
-          fullWidth
-          label="Current Status"
-          value={this.state.status}
-          onChange={this.handleChange}
-          margin="normal"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start"> </InputAdornment>
-            )
-          }}>
-          {options.map(option => (
-            <MenuItem key={option.id} value={option.value}>
-              {option.value}
-            </MenuItem>
-          ))}
-        </TextField>
         <Grid
-          style={{ marginTop: '1rem' }}
           container
           direction="row"
           justify="center"
-          alignItems="flex-start"
-          margin="normal"
-        >
-          <Button variant="contained" color="primary" >
-            Save
-          </Button>
+          alignItems="center">
+          <form
+            style={{ maxWidth: '50rem', marginTop: '5%' }}
+            onSubmit={this.handleSubmit}>
+            <Typography variant='h5' gutterBottom align='center'>
+              Create New Job Prospect
+            </Typography>
+            <TextField
+              required
+              fullWidth
+              id="company"
+              placeholder="e.g Dodge's Southern Style"
+              label="Company Name"
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              id="title"
+              placeholder="e.g Software Developer I"
+              label="Job Title"
+              margin="normal"
+            />
+            <TextField
+              id="description"
+              label="Description"
+              fullWidth
+              multiline
+              rows="5"
+              margin="normal"
+              variant="filled"
+            />
+            <TextField
+              id="details"
+              label="Details"
+              fullWidth
+              multiline
+              rows="5"
+              margin="normal"
+              variant="filled"
+            />
+            <TextField
+              select
+              fullWidth
+              id="status"
+              label="Current Status"
+              value={this.state.status}
+              onChange={this.handleChange}
+              margin="normal"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start"> </InputAdornment>
+                )
+              }}>
+              {options.map(option => (
+                <MenuItem key={option.id} value={option.value}>
+                  {option.value}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Grid
+              style={{ marginTop: '1rem' }}
+              container
+              direction="row"
+              justify="center"
+              alignItems="flex-start"
+              margin="normal"
+            >
+              <Button type="submit" variant="contained" color="default" >Create</Button>
+            </Grid>
+          </form>
         </Grid>
-      </div >
+      </div>
     )
   }
 }
